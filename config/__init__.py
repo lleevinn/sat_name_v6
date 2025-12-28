a65c50241a30a28e5d1f74a3770e7caac198a01d
lleevinn <maksimovkir@bk.ru>
2025-12-28 06:47:20 +0300

    📄 NEW: Init for config module

---
+++ config/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+config/ - Пакет настроек и конфигурации IRIS AI
+"""
+
+from .settings import IrisConfig, IntegrationsConfig, PathsConfig
+
+__all__ = ['IrisConfig', 'IntegrationsConfig', 'PathsConfig']
