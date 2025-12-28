31e0de7c0bae8056325b04bcf21825c7f9331708
lleevinn <maksimovkir@bk.ru>
2025-12-28 07:44:40 +0300

    🔄 FINAL MERGE: Объединил src/utils/ с utils/ + очистил старые файлы iris_ai/

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,7 +1,10 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+@"
 """
-utils/ - Утилиты и вспомогательные модули
+Utils Package - Вспомогательные утилиты для IRIS AI
 """
 
-__all__ = ['voice_recorder']
+from .voice_recorder import VoiceRecorder
+from .tts_utils import synthesize_and_play
+
+__all__ = ['VoiceRecorder', 'synthesize_and_play']
+"@ | Out-File -FilePath utils/__init__.py -Encoding UTF8
